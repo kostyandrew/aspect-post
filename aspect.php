70f812cf75d7b89eff9d2f1120f712103a0d8769
Andrii Kostetskyi <kostyandrew@gmail.com>
2025-03-11 00:53:09 +0200

    pre-publish changes

--- aspect.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
-/*
-Plugin Name: Aspect Post
-Version:     0.1-alpha
- */
-Aspect\Type::set('note')
-    ->setArgument('public', true);
